160b2febf7581459d335f963fcfc364586c4ed6e
faizan ahmad <faizan89ahmad@gmail.com>
2020-09-10 00:12:17 +0530

    Updates

---
+++ precache-manifest.28746d946273af56f93b2851b1724861.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947e873a9b136ef9e18f01798ab36b84",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "05225b93b454dedde802",
+    "url": "/reactlivewebsite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "f7272ef7d3736cbe88b7",
+    "url": "/reactlivewebsite/static/css/main.ae87e65f.chunk.css"
+  },
+  {
+    "revision": "05225b93b454dedde802",
+    "url": "/reactlivewebsite/static/js/2.64c72e70.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactlivewebsite/static/js/2.64c72e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7272ef7d3736cbe88b7",
+    "url": "/reactlivewebsite/static/js/main.b7e0d3f8.chunk.js"
+  },
+  {
+    "revision": "d0f68cc663c16990ee86",
+    "url": "/reactlivewebsite/static/js/runtime-main.7285ad41.js"
+  },
+  {
+    "revision": "e3da5f6c65fbdb898f2091a37df2ff17",
+    "url": "/reactlivewebsite/static/media/about-1.e3da5f6c.jpg"
+  },
+  {
+    "revision": "d3affe8b1add411a5cf400993b062fbc",
+    "url": "/reactlivewebsite/static/media/bus-a.d3affe8b.jpg"
+  },
+  {
+    "revision": "db27a4be2a746319eb915aebddbd1e19",
+    "url": "/reactlivewebsite/static/media/i-a.db27a4be.jpg"
+  },
+  {
+    "revision": "d8c041e5ba2887ba71737522f9c617b2",
+    "url": "/reactlivewebsite/static/media/i-b.d8c041e5.jpg"
+  },
+  {
+    "revision": "4fbe4b587eafcbfae1c8f715c41265f1",
+    "url": "/reactlivewebsite/static/media/i-c.4fbe4b58.jpg"
+  },
+  {
+    "revision": "e68ae629403bc9adc4361fb84cbf16a9",
+    "url": "/reactlivewebsite/static/media/i-d.e68ae629.jpg"
+  },
+  {
+    "revision": "37416858087fb35aabe7731f960a8916",
+    "url": "/reactlivewebsite/static/media/web-1.37416858.jpg"
+  },
+  {
+    "revision": "4a4bbd80636c7e0a56f9c7e75a83cec9",
+    "url": "/reactlivewebsite/static/media/web-2.4a4bbd80.jpg"
+  }
+]);
